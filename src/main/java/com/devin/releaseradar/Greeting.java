321a43da79814ab164e874640a13f46bc6317b4e
Devin Riess <devin.riess@colorado.edu>
2024-05-21 12:37:53 -0600

    interactive home page

---
+++ src/main/java/com/devin/releaseradar/Greeting.java
@@ -0,0 +1,17 @@
+package com.devin.releaseradar;
+
+
+public class Greeting {
+
+    private String name;
+    
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ src/main/java/com/devin/releaseradar/Home.java
@@ -0,0 +1,30 @@
+package com.devin.releaseradar;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@Controller
+public class Home {
+    
+
+    @GetMapping("/greeting")
+    public String getMethodName(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+    
+    @PostMapping("/greeting")
+    public String postMethodName(@ModelAttribute Greeting greeting, Model model) {
+        //TODO: process POST request
+        model.addAttribute("greeting", greeting);
+        return "result";
+    }
+    
+
+}
